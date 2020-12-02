b71f8e58943588c835d8fb5b7d623a688704533e
Justin Hammond <justin.hammond@rackspace.com>
2020-12-02 23:03:53 +0000

    Added requests

--- events_test.go
+++ events_test.go
@@ -1,6 +1,8 @@
 package catchall_test
 
 import (
+	"log"
+	"net/http"
 	"testing"
 
 	"github.com/mailgun/catchall"
@@ -8,10 +10,25 @@ import (
 
 func BenchmarkEventBus(b *testing.B) {
 	bus := catchall.SpawnEventPool()
+	client := &http.Client{}
 
 	b.Run("GetEvent", func(b *testing.B) {
 		for n := 0; n < b.N; n++ {
 			e := bus.GetEvent()
+			uri_root := "http://localhost:8080/events/" + e.Domain + "/"
+			action := "delivered"
+			if e.Type == catchall.TypeBounced {
+				action = "bounced"
+			}
+			req, err := http.NewRequest(http.MethodPut, uri_root+action, nil)
+			if err != nil {
+				log.Fatal(err)
+			}
+			resp, err := client.Do(req)
+			resp.Body.Close()
+			if err != nil {
+				log.Fatal(err)
+			}
 			bus.RecycleEvent(e)
 		}
 	})
